965fccb798b3e71d1c85c2ebf6daa244664d10c4
BOOPATHI7582 <boopathisiva990@gmail.com>
2024-07-29 20:24:30 +0530

    first commit

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -1,6 +1,6 @@
-import { BiLogoCss3, BiLogoFirebase, BiLogoMongodb, BiLogoReact, BiLogoRedux, BiLogoTailwindCss, BiLogoTypescript, } from "react-icons/bi";
-import { TbBrandNextjs, TbApi, TbDatabase, TbBrandPrisma } from "react-icons/tb";
-import { SiThemoviedatabase, SiExpress } from "react-icons/si";
+import { BiLogoCss3,  BiLogoReact,} from "react-icons/bi";
+
+import { SiThemoviedatabase, } from "react-icons/si";
 
 const ProjectsData = [
   {
